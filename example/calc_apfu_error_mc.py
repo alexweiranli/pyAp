e5be4a3f70537066ffe73f5497a768cd8a2bb899
Eason Zhang <yishen.zhang@kuleuven.be>
2024-07-15 12:46:25 -0500

    update pd concat, docs, html

--- example/calc_apfu_error_mc.py
+++ example/calc_apfu_error_mc.py
@@ -15,8 +15,8 @@ data = pd.read_excel('data_ap_major_volatile.xlsx')
 # read errors (could be in the same file as the concentrations)
 errors = pd.read_excel('data_ap_errors.xlsx')
 
-oxides = {'SIO2','TIO2','AL2O3','FEO','CAO','MGO','MNO','K2O','NA2O','P2O5','SO3','CO2', 'F','CL','H2O','CE2O3','SRO'}
-col = oxides.intersection(list(data))
+col = ['SIO2','TIO2','AL2O3','FEO','CAO','MGO','MNO','K2O','NA2O','P2O5','SO3','CO2', 'F','CL','H2O','CE2O3','SRO']
+
 
 ## mc
 # set up mc for error calculation
@@ -26,7 +26,7 @@ print('>> MC = ' , str(mc) , '. Sampling starts ...')
 ap_mc_stoic = pd.DataFrame([]); ap_mc_res = pd.DataFrame([])
 for i in range(len(data)):
     df_iter = ap_mc(data[col], errors[col], i, mc)  # sample values within uncertainty ranges for "mc" times
-    ap_mc_stoic = ap_mc_stoic.append(df_iter)
+    ap_mc_stoic = pd.concat([ap_mc_stoic, df_iter])
   
 ap_mc_stoic.columns = col
 
@@ -34,9 +34,9 @@ ap_mc_stoic.columns = col
 ap_mc_stoic['sample'] = data.loc[data.index.repeat(mc)]['sample']
 ap_mc_stoic.reset_index(inplace=True, drop=True)
 
-## calculate stoichiometry for all mc samples 
+## calculate stoichiometry for all mc samples
 ap_mc_res = stoi_(ap_mc_stoic)
 
-print('\n>> Simulation completed. Results saved to "mc_stoic.csv". \n') 
+print('\n>> Simulation completed. Results saved to "mc_stoic.csv". \n')
 ap_mc_res.groupby('sample').agg(
-    ['mean','median', 'std']).to_csv('mc_stoic.csv')
+    ['mean','median', 'std']).to_csv('mc_stoic_1507.csv')
